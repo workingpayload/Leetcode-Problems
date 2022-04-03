d18975292ffc31307ee8c32e0ef64466cced88b7
Raj <51259329+workingpayload@users.noreply.github.com>
2022-04-03 10:41:40 +0530

    Time: 39 ms (76.09%), Space: 13.9 MB (31.90%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -5,6 +5,10 @@ class Solution:
         nums = []
         for i in a:
             nums.append(i)
+        
+        if len(nums)==2:
+            if nums[0]!=nums[1]:
+                flag = 0    
 
         for i in range(len(nums)-1):
             if nums[i]!=nums[i+1]:
