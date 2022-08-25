13f08066f251cb9cb3774a0bd859ecdb613ad175
Raj <51259329+workingpayload@users.noreply.github.com>
2022-08-26 00:25:03 +0530

    Time: 1415 ms (18.61%), Space: 27.5 MB (98.41%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,13 +2,14 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
         local = 0
-        global_max = float('-inf')
+        global_ = float('-inf')
         
         for i in range(len(nums)):
             
             local = max(nums[i],nums[i]+local)
-
-            if local>global_max:
-                global_max = local
+            if local>global_:
+                global_ = local
                 
-        return global_max        
+        return global_        
+        
+        
