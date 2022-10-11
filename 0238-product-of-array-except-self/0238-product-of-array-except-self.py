3c886202a738edd21f3398de94cf641917395ac7
Raj <51259329+workingpayload@users.noreply.github.com>
2022-10-11 17:41:44 +0530

    Time: 242 ms (92.41%), Space: 21.2 MB (82.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,39 +3,20 @@ class Solution:
         
         ans = [1]*len(nums)
         
-        prefix = [1]*len(nums)
+        prefix = 1
         
-        postfix = [1]*len(nums)
+        postfix = 1
         
         
         for i in range(0,len(nums)):
             
-            if i==0:
-                prefix[i] = prefix[i]*nums[i]
-            else:
-                prefix[i] = prefix[i-1]*nums[i]
-            
-            
+            ans[i] = prefix
+            prefix*=nums[i]
+        
         for i in range(len(nums)-1,-1,-1):
             
-            if i==len(nums)-1:
-                postfix[i]= postfix[i]*nums[i]
-            else:
-                postfix[i] = postfix[i+1]*nums[i]
-            
-            
-            
-        for i in range(len(nums)):
-            
-            if i==0:
-                ans[i]=postfix[i+1]
-                
-            elif i==len(ans)-1:
-                ans[i]=prefix[i-1]
-                
-            else:
-                ans[i]=prefix[i-1]*postfix[i+1]
-                
-       
+            ans[i] = ans[i]*postfix
+            postfix*=nums[i]
+           
         return ans
         
