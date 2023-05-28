2d4ee4be026dc5d684b2076f5d3578ca6c2a7baa
Raj <51259329+workingpayload@users.noreply.github.com>
2023-05-28 12:40:11 +0530

    Time: 68 ms (86.55%), Space: 45.7 MB (69.91%) - LeetHub

---
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -0,0 +1,25 @@
+var areDeeplyEqual = function(o1, o2) {
+    const type1 = typeof(o1);
+    const type2 = typeof(o2);
+    const isArray1 = Array.isArray(o1);
+    const isArray2 = Array.isArray(o2);
+    // if both params are different type then return false
+    if((type1 !== type2) || (isArray1 !== isArray2)) return false;
+    // if params are primitive then check equality
+    if((type1 !== 'object') || (o1 === null)) return o1 === o2;
+    // if params are arrays
+    if(isArray1){
+      if(o1.length !== o2.length) return false;
+      for(let i=0;i<o1.length;i++){
+        if(!areDeeplyEqual(o1[i],o2[i])) return false;
+      }
+    }
+    // if params are array
+    const key1 = Object.keys(o1);
+    const key2 = Object.keys(o2);
+    if(key1.length !== key2.length) return false;
+    for(let i=0;i<key1.length;i++){
+      if(!areDeeplyEqual(o1[key1[i]],o2[key1[i]])) return false;
+    }
+    return true;
+};
