6e2cce24afabd558c5c552357d7bc65e4d1afacd
Raj <51259329+workingpayload@users.noreply.github.com>
2023-07-11 14:29:38 +0530

    Time: 55 ms (54.51%), Space: 16.7 MB (81.44%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,40 @@
+class Solution:
+    def distanceK(self, root, target, K):
+        # Step 1: Build adjacency list graph
+        graph = {}
+        self.buildGraph(root, None, graph)
+
+        # Step 2: Perform BFS from the target node
+        queue = [(target, 0)]
+        visited = set([target])
+        result = []
+        
+        while queue:
+            node, distance = queue.pop(0)
+            
+            if distance == K:
+                result.append(node.val)
+                
+            if distance > K:
+                break
+            
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append((neighbor, distance + 1))
+        
+        return result
+    
+    def buildGraph(self, node, parent, graph):
+        if not node:
+            return
+        
+        if node not in graph:
+            graph[node] = []
+            
+        if parent:
+            graph[node].append(parent)
+            graph[parent].append(node)
+            
+        self.buildGraph(node.left, node, graph)
+        self.buildGraph(node.right, node, graph)
