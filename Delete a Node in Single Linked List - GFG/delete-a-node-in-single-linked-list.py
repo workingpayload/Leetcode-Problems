2ab336c2d446c49aa353acc951f3f5af64369469
Raj <51259329+workingpayload@users.noreply.github.com>
2023-08-30 22:36:11 +0530

    Added solution - LeetHub

---
+++ Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.py
@@ -0,0 +1,80 @@
+# your task is to complete this function
+# function should return new head pointer
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def delNode(head, k):
+    # Code here
+    temp = head
+    n = 1
+    
+    if(k==1):
+        return head.next
+    
+    while(head.next):
+        if(n+1==k):
+            if(head.next and head.next.next):
+                head.next = head.next.next
+            else:    
+                head.next = None    
+        n+=1
+        if(head.next):
+            head = head.next
+        
+    return temp    
+#{ 
+ # Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.last = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+            self.last = self.head
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.last.next = new_node
+            self.last = new_node
+            # self.
+            # temp = self.head
+            # while(temp.next):
+            #     temp=temp.next
+            # temp.next = new_node
+
+def printlist(head):
+    temp = head
+    while(temp):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        k = int(input())
+        newhead = delNode(list1.head, k)
+        printlist(newhead)
+# Contributed By: Harshit Sidhwa
+# } Driver Code Ends
